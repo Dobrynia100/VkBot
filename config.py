0f83087522078d9f46f22b0419f1bfc0513fabee
Dobrynia100 <58748374+Dobrynia100@users.noreply.github.com>
2022-11-08 02:27:59 +0300

    security changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
     'CLIENT_ID':'123',
     'SECRET_CODE':'123',
-    'TOKEN':'vk1.a.2Vfg8NAtWBdT_nrXgxLriOWLb96NunT7YY42atwNM4Kk1mu1_XTFMyu72kIhIfSMHNM1EOSepom3vceyeDLx24WiNGNTLYau9ri-EbeUqCeKGzJyP474Kn5hFlOXlKpuPswLk7_rL36ckQYs-2Bea-zv_OFDOYd8j12Sl8Zt8fYZTn3qvO2aJP6WrQYHw0u5',
-    'VKTOKEN':'vk1.a.QU5dPoCWqx8GRNOEaZHA9wNJZrPf8v4nxXXoqY_ce_ZqNOOSGflFQF6C555MePe_a7eqq4Ys_9thNiazBwdnjhdFgKrtT9tbmLxCi3ctSpaYB-0Fp1B763OSf8yyqbXIrHFsKROITEtKKhbo3fV9AkztrHQ2j9xF37Y1Z54kB4lPZCXZHzmgtJnQFVRtyJXV'
+    'TOKEN':'your-token',
+   
 }
